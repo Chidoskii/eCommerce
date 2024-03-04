9b805661d1d0e6372ac2ffa68129f66a663d2a06
Ramon Plascencia <rplascencia@artemis.cs.csub.edu>
2024-03-03 20:26:24 -0800

    Updated backend.php to be a function file instead of a page

--- php/backend.php
+++ php/backend.php
@@ -1,108 +1,88 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>
-		test
-		</title>
-	</head>
-	<body>
-		<p>Testing</p>
+<?php
+// Sorting by Price
+function sort_price($order) {
+	$sql = "SELECT pID, prod_desc, rating, price FROM Products ORDER BY price ?";
+	$query = $db->prepare($sql);
+	$query->bind_param("s", $order);
+	$query->execute();
+	$result = $query->get_result();
 
-	<?php
-	$servername = "localhost";
-	$username = "rplascencia";
-	$password = "bov3Veck";
-	$database = "rplascencia";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $database);
-	if ($conn->connect_error) {
-		die("failed connection " . $conn->connect_error);
-	}
-
-	echo "Price search<br><br>";
-
-	$sql = "SELECT pID, prod_desc, rating, price FROM Products ORDER BY price";
-	$result = $conn->query($sql);
-
+	/* Prints the result
 	if($result->num_rows > 0){
 	while($row = $result->fetch_assoc()) {
 		echo $row["prod_desc"]." rating: " .
 		$row["rating"] . " price: $" . $row["price"] . "<br>";
 	}
-	} else {
-		echo "Failed";
-	}
+}*/
+}
 
-	echo "<br>Rating search<br><br>";
-
+//sort by rating
+function sort_rating($order) {
 	$sql = "SELECT pID, prod_desc, rating, price FROM Products ORDER BY rating";
-	$result = $conn->query($sql);
+	$query = $db->prepare($sql);
+	$query->bind_param("s", $order);
+	$query->execute();
+	$result = $query->get_result();
 
+	/* Prints result
 	if($result->num_rows > 0){
 	while($row = $result->fetch_assoc()) {
 		echo $row["prod_desc"]." rating: " .
 		$row["rating"] . " price: $" . $row["price"] . "<br>";
 	}
-	} else {
-		echo "Failed";
-	}
+}*/
+}
 
-	echo "<br>Category filter<br><br>";
-
+// Filter by Category
+function filter_category($category) {
 	$sql = "SELECT pID, category, prod_desc, rating, price FROM Products WHERE category= ?";
-	$category = "Electronics";
 	$query = $conn->prepare($sql);
 	$query->bind_param("s", $category);
 	$query->execute();
 	$result = $query->get_result();
 
+	/* prints result
 	if($result->num_rows > 0){
 	while($row = $result->fetch_assoc()) {
 		echo $row["prod_desc"]." rating: " .
 		$row["rating"] . " price: $" . $row["price"] . "<br>";
 	}
-	} else {
-		echo "Failed";
-	}
+}*/
+}
 
-	echo "<br>Vendor filter: Doll Le Fevre<br><br>";
-
+// Filter by vendor
+function filter_vendor($vendor) {
 	$sql = "SELECT Vendors.vendor_name, Products.pID, Products.prod_desc, Products.rating, 
 	Products.price FROM Vendors RIGHT JOIN Products 
 	ON Vendors.vID = Products.vID WHERE Vendors.vendor_name= ? ORDER BY Products.pID";
-	$category = "Doll Le Fevre";
 	$query = $conn->prepare($sql);
-	$query->bind_param("s", $category);
+	$query->bind_param("s", $vendor);
 	$query->execute();
 	$result = $query->get_result();
 
+	/*prints the result
 	if($result->num_rows > 0){
 	while($row = $result->fetch_assoc()) {
 		echo $row["prod_desc"]." rating: " .
 		$row["rating"] . " price: $" . $row["price"] . "<br>";
 	}
-	} else {
-		echo "Failed";
-	}
+}*/
+}
 
-	/*echo "<br>Your Order:<br><br>";
+	/*Print the orders
 	$sql = "Select"*/
 
-	echo "<br>Total: ";
+//Function to print total
+function total($ID) {
 	$sql = "SELECT SUM(total) FROM Orders WHERE uID=?";
-	$ID = '1';
 	$query = $conn->prepare($sql);
 	$query->bind_param("i", $ID);
 	$query->execute();
 	$result = $query->get_result();
 	
+	/*Prints result
 	$row = $result->fetch_assoc();
 	$value = $row["SUM(total)"];
-	echo $value;
-
-	$conn->close();
-	?>
-	
-	</body>
-</html>
+	echo $value;*/
+}
+?>
