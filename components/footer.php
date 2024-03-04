baa5816589d727cd267901b66b88eb2f25407137
Chidi Okpara <cokpara@csub.edu>
2024-03-04 10:33:22 -0800

    add component folder. hide config file

---
+++ components/footer.php
@@ -0,0 +1,42 @@
+<footer>
+
+  <div class="container-fluid footy-can">
+    <div class="footer-links-can">
+      <div class="footy-links-l">
+        <p class="footy-sub-header">More About Us</p>
+        <a class="footy-link-list" href="#">Careers</a>
+        <a class="footy-link-list" href="#">eRowdy Newsletter</a>
+        <a class="footy-link-list" href="#">Executives</a>
+        <a class="footy-link-list" href="#">For Developers</a>
+      </div>
+      <div class="footy-links-m">
+        <p class="footy-sub-header">Buisness Opportunities</p>
+        <a class="footy-link-list" href="#">Sell Apps</a>
+        <a class="footy-link-list" href="#">Vendors</a>
+        <a class="footy-link-list" href="#">Advertisement</a>
+        <a class="footy-link-list" href="#">Partners & Affiliates</a>
+      </div>
+      <div class="footy-links-r">
+        <p class="footy-sub-header">Customer Support</p>
+        <a class="footy-link-list" href="#">Your Account</a>
+        <a class="footy-link-list" href="#">Your Orders</a>
+        <a class="footy-link-list" href="#">Your Cart</a>
+        <a class="footy-link-list" href="#">Help</a>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      &copy;&nbsp;
+      <div class="erowdy-copyright">
+      <span class="brand-letter-e">e</span>
+      <span class="brand-letter-r">R</span>
+      <span class="brand-letter-o">o</span>
+      <span class="brand-letter-w">w</span>
+      <span class="brand-letter-d">d</span>
+      <span class="brand-letter-y">y</span>
+      </div>
+      &nbsp;
+      2024
+    </div>
+  </div>
+
+</footer>
